eb96af02f649ca3dac9d9f5373a883e31434eb59
Saagar Jha <saagar@saagarjha.com>
2022-12-05 23:34:09 -0800

    2022 Day 6

---
+++ 2022/06/script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env aoc_repl.py
+
+from collections import Counter
+import inspect
+import operator
+import os
+import pathlib
+import sys
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.insert(0, os.path.dirname(os.path.dirname(currentdir)))
+
+from aoc import *
+
+input_filename = "input"
+if "AOC_SAMPLE" in os.environ:
+	input_filename = "sample"
+
+input = pathlib.Path(input_filename).read_text().strip()
+
+i = 0
+while i < input.len:
+	if set(input[max(0, i - 4): i]).len == 4:
+		print(i)
+	i += 1
